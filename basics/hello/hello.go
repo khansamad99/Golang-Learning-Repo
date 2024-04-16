30707327136a77f2e80ca86c66e8a54c529c9c05
Samad Khan <samadk619@gmail.com>
2024-04-17 00:06:44 +0530

    lrc-cache

--- basics/hello/hello.go
+++ basics/hello/hello.go
@@ -2,19 +2,38 @@ package main
 
 import (
 	"fmt"
-	"log"
-
-	"example.com/greetings"
 )
 
 func main() {
-	log.SetPrefix("greetings: ")
-	log.SetFlags(0)
-	// Get a greeting message and print it.
-	message, err := greetings.Hello("")
+	// log.SetPrefix("greetings: ")
+	// log.SetFlags(0)
+	// // Get a greeting message and print it.
+	// message, err := greetings.Hello("")
 
-	if err != nil {
-		log.Fatal(err)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(message)
+
+	var x [5]int = [5]int{1, 2, 3, 4, 5}
+
+	var s []int = x[1:2]
+	fmt.Println(x)
+	fmt.Println(s)
+
+	var m map[string]int = map[string]int{
+		"apple":  1,
+		"mango":  2,
+		"orange": 5,
 	}
-	fmt.Println(message)
+
+	fmt.Println(m)
+
+	v, ok := m["papapya"]
+
+	if ok {
+		fmt.Println(v)
+	} else {
+		fmt.Println(ok)
+	}
 }
---
+++ go-lrucache/go.mod
@@ -0,0 +1,3 @@
+module go-lrcucache
+
+go 1.22.1
